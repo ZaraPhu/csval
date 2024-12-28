d3551497e8a37d80d6b1c1bcc8a1554be1b96b40
Zara <zara@Zaras-MacBook-Air.local>
2024-12-28 19:57:02 -0400

    added functionality for checking md5 sums, restructured control flow to improve behaviour with optional arguments, changes to sha256 flag name (sha256sum -> sha256)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 use clap::Parser;
-use sha256::try_digest;
-use std::path::Path;
+use std::fs;
+use std::path::PathBuf;
 
 // A program that outputs True if the given file has a sha256 struct equal to the sha56 digest passed into the command
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[derive(Parser, Default)]
+#[command(version, about)]
 struct Args {
-    // The file to be comapred
-    #[arg(short, long)]
-    file_path: String,
+    // The fie path which is being checked
+    #[arg(required = true)]
+    file_path: PathBuf,
 
-    // The provided sha256 sum
-    #[arg(short, long)]
-    sha256sum: String,
+    // Providing an md5 sum
+    #[arg(long, required = false)]
+    md5: Option<String>,
+
+    // Providing a sha256 sum
+    #[arg(long, required = false)]
+    sha256: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let file_path = Path::new(&args.file_path);
-    let computed_sum = try_digest(file_path).expect("Expected a file path.");
+    let file_data = fs::read(&args.file_path)
+        .expect("Error: The provided file could not be converted into a bytes vector.");
 
-    if args.sha256sum == computed_sum {
-        println!("Got a match!");
-    } else {
-        println!("Not a match.");
+    match args.md5 {
+        Some(md5_given) => {
+            let md5_sum = format!("{:x}", md5::compute(&file_data));
+            if md5_given == md5_sum {
+                println!("Md5 sums: Match.")
+            } else {
+                println!("Md5 sums: Mismatch");
+            }
+        }
+        None => {}
+    }
+
+    match args.sha256 {
+        Some(sha256_given) => {
+            let sha256_sum = sha256::try_digest(&args.file_path)
+                .expect("Error: unable to compute sha256 sum for file {args.file_path}.");
+            if sha256_given == sha256_sum {
+                println!("SHA256 sums: Match.");
+            } else {
+                println!("SHA256 sums: Mismatch.");
+            }
+        }
+        None => {}
     }
 }
